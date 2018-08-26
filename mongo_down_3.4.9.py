55a537ac4b69e7e783584edabaa8ab94fd7ad719
liaoxuegithup <1335992113@qq.com>
2018-08-26 08:04:42 +0800

    mongon_down

---
+++ mongo_down_3.4.9.py
@@ -0,0 +1,10 @@
+1\wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel70-3.4.9.tgz
+
+tar zxf mongodb-linux-x86_64-rhel70-3.4.9.tgz
+
+mkdir -p /data/db
+
+./mongod
+
+ ./mongo
+show dbs
